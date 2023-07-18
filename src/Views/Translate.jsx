e2e89e5fe3f5ad4e858b1573fbba2c064d1edcf3
victorlun <victor.lundqvist@se.experis.com>
2023-07-18 13:50:23 +0200

    Image translation implemented :tada:

--- src/Views/Translate.jsx
+++ src/Views/Translate.jsx
@@ -5,9 +5,8 @@ function Translate () {
     return(
         <>
         <h1>TRANSLATE</h1>
-        <p style={{textAlign: "center"}}>Here you can translate from English to Sign Language!</p>
+        <p style={{textAlign: "center", fontfamily: 'Courier New', fontSize: "28px"}}>Here you can translate from English to Sign Language!</p>
         <TranslateForm/>
-
         </>
     )
 
